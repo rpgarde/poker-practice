0992a8a0cb08c496eb7d15ec66aa93eb5796c4de
rpgarde <rpgarde@gmail.com>
2021-12-06 02:04:05 +1100

    458 cases scored

--- index.js
+++ index.js
@@ -22,119 +22,117 @@ myInterface.on('line', function (line) {
     console.log(`Player 1: ${player1}`)
 
     const evaluatedPlayer1 = handEvaluator(player1)
-    console.log(evaluatedPlayer1)
+    // console.log(evaluatedPlayer1)
 
     let player2 = line.slice(-15).trim()
     console.log(`Player 2: ${player2}`)
 
     const evaluatedPlayer2 = handEvaluator(player2)
-    console.log(evaluatedPlayer2)
+    // console.log(evaluatedPlayer2)
 
     // 10 royal flush check 
     if(handEvaluator(player1).royalFlush&&!evaluatedPlayer2.royalFlush){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a Royal Flush')
         player1Score++
     }
     else if(!evaluatedPlayer2.royalFlush&&evaluatedPlayer2.royalFlush){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Royal Flush')
         player2Score++
     }
     //TODO: Add evaluations for when they are equal
     // 9 straight flush check
     else if (evaluatedPlayer1.straightFlush&&!evaluatedPlayer2.straightFlush){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a Straight Flush')
         player1Score++
     }
     else if (!evaluatedPlayer1.straightFlush&&evaluatedPlayer2.straightFlush){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Straight Flush')
         player2Score++
     }
     // 8 four of a kind check 
     else if (evaluatedPlayer1.fourOfAKind&&!evaluatedPlayer2.fourOfAKind){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a 4 of a Kind')
         player1Score++
     }
     else if (!evaluatedPlayer1.fourOfAKind&&evaluatedPlayer2.fourOfAKind){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a 4 of a Kind')
         player2Score++
     }
     // 7 full house check 
     else if (evaluatedPlayer1.fullHouse&&!evaluatedPlayer2.fullHouse){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a Full House')
         player1Score++
     }
     else if (!evaluatedPlayer1.fullHouse&&evaluatedPlayer2.fullHouse){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Full House')
         player2Score++
     }
     // 6 flush 
     else if (evaluatedPlayer1.flush&&!evaluatedPlayer2.flush){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a Flush')
         player1Score++
     }
     else if (!evaluatedPlayer1.flush&&evaluatedPlayer2.flush){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Flush')
         player2Score++
     }
     // 5 straight check
     else if (evaluatedPlayer1.straight&&!evaluatedPlayer2.straight){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a Straight')
         player1Score++
     }
     else if (!evaluatedPlayer1.straight&&evaluatedPlayer2.straight){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Straight')
         player2Score++
     }
     // 4 three of a kind
     else if (evaluatedPlayer1.threeOfAKind&&!evaluatedPlayer2.threeOfAKind){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a 3 of a Kind')
         player1Score++
     }
     else if (!evaluatedPlayer1.threeOfAKind&&evaluatedPlayer2.threeOfAKind){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Flush')
         player2Score++
     }
     // 3 two pairs 
     else if (evaluatedPlayer1.twoPairs&&!evaluatedPlayer2.twoPairs){
-        console.log('player 1 won')
+        console.log('Player 1 won due to Two Pairs')
         player1Score++
     }
     else if (!evaluatedPlayer1.twoPairs&&evaluatedPlayer2.twoPairs){
-        console.log('player 2 won')
+        console.log('Player 2 won due to Two Pairs')
         player2Score++
     }   
     // 2 pair 
     else if (evaluatedPlayer1.pair&&!evaluatedPlayer2.pair){
-        console.log('player 1 won')
+        console.log('Player 1 won due to a Pair')
         player1Score++
     }
     else if (!evaluatedPlayer1.pair&&evaluatedPlayer2.pair){
-        console.log('player 2 won')
+        console.log('Player 2 won due to a Pair')
         player2Score++
     }  
     // 1 highest card value
     else if (evaluatedPlayer1.highestCardValue>evaluatedPlayer2.highestCardValue){
-        console.log('player 1 won')
+        console.log('Player 1 won due their highest card value')
         player1Score++
     }
     else if (evaluatedPlayer1.highestCardValue<evaluatedPlayer2.highestCardValue){
-        console.log('player 2 won')
+        console.log('Player 2 won due their highest card value')
         player2Score++
     }
     else {
-        console.log('Exception. No one won!')
+        console.log('Exception. No one won! I still have work to do in my code.')
         exceptionCount++
     }
     // Now everything gets tallied
-    console.log(`\nFINAL SCORES:\n`)
+    if(roundNumber==500){
+    console.log(`=============\nFINAL SCORES:\n=============`)
     console.log(`Player 1: ${player1Score}`)
     console.log(`Player 2: ${player2Score}`)
     console.log(`Unscored count: ${exceptionCount}`)
-    return
+    }
 });
-
-
-
 
 // Player 1 and Player 2's hand each get evaluated 
 
